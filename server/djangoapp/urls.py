59c23b4d644233d1d7f8428f2ae387ff72aaad49
manasamadhavi <manasa.m397@gmail.com>
2023-07-18 02:14:28 -0400

    week2

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,11 +14,11 @@ urlpatterns = [
     # path for contact us view
     path(route = 'contact/', view = views.contact, name = 'contact'),
     # path for registration
-
+    path(route = 'signup/', view = views.signup, name = 'signup'),
     # path for login
-
+    path(route = 'login/', view = views.login, name = 'login'),
     # path for logout
-
+    path(route = 'logout/', view = views.logout, name = 'logout'),
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
